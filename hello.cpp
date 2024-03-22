0e45ffec6ba5dcbeff962dbf0eac381cf13318c7
minseokH <dhnui013@naver.com>
2024-03-22 12:47:05 +0900

    assign

--- hello.cpp
+++ hello.cpp
@@ -1,11 +1,11 @@
 #include<iostream>
-
+ii
 using namespace std;
 
 int main(){
 
     cout<<"Hello World!"<<endl;
-    cout<<"My Name is ~~"<<endl;
+    cout<<"My Name is Hwang MinSeok"<<endl;
     return 0;
 
 }
